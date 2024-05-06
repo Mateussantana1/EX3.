ee5dc5b4aa6f1b7a027f4fc3d97b13ef340d1f87
Mateussantana1 <169102554+Mateussantana1@users.noreply.github.com>
2024-05-06 19:24:17 -0300

    Add files via upload
    Mateus Santana

---
+++ EX3.js
@@ -0,0 +1,22 @@
+Nota = prompt ("digite sua nota");
+
+if (Nota == 10){
+console.log ("APROVADO com nota A")
+} else if (Nota == 9){
+    console.log ("APROVADO com nota B")
+} else if (Nota == 8){
+    console.log ("APROVADO com nota C")  
+} else if (Nota == 7){
+    console.log ("APROVADO com nota D")
+} else if (Nota == 6){
+    console.log ("APROVADO com nota E")   
+} else if (Nota == 5 ){
+    console.log ("REPROVADO com nota F")
+} else if (Nota == 4 ){ 
+console.log ("REPROVADO com nota G")
+}else if (Nota == 3 ){ 
+    console.log ("REPROVADO com nota H")
+}else if (Nota == 2 ){ 
+    console.log ("REPROVADO com nota I")
+ }    else 
+    console.log ("REPROVADO");
